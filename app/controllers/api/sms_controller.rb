729abaedecab4f1ad88ab2190e6c2557366133f9
Ben Gully <gullybenjamin@yahoo.com>
2023-07-07 10:14:45 -0400

    Add empty send message action

---
+++ app/controllers/api/sms_controller.rb
@@ -0,0 +1,4 @@
+class Api::SmsController < ApplicationController
+    def send_message
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    post :send_message, to: 'sms#send_message'
+  end
 end
